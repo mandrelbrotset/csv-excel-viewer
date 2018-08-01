18a8f9fff07cb5b26b12cdb35b8c8b020dd0e89d
Sheriff Olaoye <sheriffolaoye@gmail.com>
2018-08-01 00:49:26 -0500

    modified code to remove vertical labels

--- viewer.py
+++ viewer.py
@@ -18,32 +18,43 @@ class main(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
         self.show()
 
     def openFile(self):
+        # declare accepted file types
         supportedFileTypes = "csv files (*.csv);;excel files (*.xls*)"
-        name, fileType = QtWidgets.QFileDialog.getOpenFileName(self, "Open File", "",supportedFileTypes)
-        print(fileType)
-        if fileType == "excel files (*.xls*)":
-            print("type is excel")
-            self.f = pd.read_excel(name)
-        elif fileType == "csv files (*.csv)":
-            print("type is csv")
-            self.f = pd.read_csv(name)
 
+        name, fileType = QtWidgets.QFileDialog.getOpenFileName(self, "Open File", "", supportedFileTypes)
+        # if the file is an excel file, use an excel parser
+        if fileType == "excel files (*.xls*)":
+            #debug print("type is excel")
+            self.f = pd.read_excel(name)
+        # if the file is an csv file, use a csv parser
+        elif fileType == "csv files (*.csv)":
+            #debug print("type is csv")
+            self.f = pd.read_csv(name)
+        # open the file if a file was selected
         if len(name) > 0:
+            # hide the "Open file" button
             self.openFileButton.setHidden(True)
+            # call method to render the file
             self.setupTable()
 
     def setupTable(self):
+        # get the header of the table in the selected file
         self.header = list(self.f.dtypes.index)
 
         self.tableWidget.setGeometry(QtCore.QRect(2, 2, 1000, 600))
+        # set the number of rows in the table to the number of rows in the file
         self.tableWidget.setRowCount(self.f.shape[0])
+        # set the number of columns to the number of columns in the file
         self.tableWidget.setColumnCount(self.f.shape[1])
         self.tableWidget.setObjectName("tableWidget")
         self.vBoxLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.vBoxLayout.addWidget(self.tableWidget)
         self.setLayout(self.vBoxLayout)
 
+        # set horizontal headers to the header of the table in the file
         self.tableWidget.setHorizontalHeaderLabels(self.header)
+        # hide the row numbers
+        self.tableWidget.verticalHeader().setHidden(True)
 
         col = 0
         for index, row in self.f.iterrows():
@@ -52,7 +63,7 @@ class main(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
                 col += 1
             col = 0
 
-        self.openFileButton.setHidden(True)
+        # show the widget with the table
         self.tableWidget.setVisible(True)
         
 if __name__=="__main__":
